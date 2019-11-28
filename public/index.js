76d76c8ad31c19a8ad01b82bc84e07e432d51901
Nikita Sidorov <NS18091@gmail.com>
2019-11-28 09:52:50 +0200

    asdqwd

--- public/index.js
+++ public/index.js
@@ -1,28 +1,26 @@
 function name_validation() {
   const regex = /^[A-Z][a-z]+/;
   if (
-    regex.test(document.getElementById("name").value) &&
-    document.getElementById("name").value.length <= 16
-  )
-    alert("Имя корректное!");
-  else alert("Имя не корректное!");
+    regex.test(document.getElementById('name').value)
+    && document.getElementById('name').value.length <= 16
+  ) alert('Имя корректное!');
+  else alert('Имя не корректное!');
 }
 
 function email_validate() {
-  const x = document.getElementById("email").value;
-  const atpos = x.indexOf("@");
-  const dotpos = x.lastIndexOf(".");
-  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length)
-    alert("Не корректный адрес e-mail!");
-  else alert("Адрес корректный!");
+  const x = document.getElementById('email').value;
+  const atpos = x.indexOf('@');
+  const dotpos = x.lastIndexOf('.');
+  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) alert('Не корректный адрес e-mail!');
+  else alert('Адрес корректный!');
 }
 
 function number_validate() {
-  input = document.getElementById("mob_num").value;
-  const cor_sym = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  input = document.getElementById('mob_num').value;
+  const cor_sym = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   let ok = true;
   if (input.length > 12) ok = false;
-  if (input.charAt(0) !== "3" || input.charAt(1) !== "8") ok = false;
+  if (input.charAt(0) !== '3' || input.charAt(1) !== '8') ok = false;
   for (let i = 2; i < input.length; i++) {
     const chr = input.charAt(i);
     let found = false;
@@ -32,8 +30,8 @@ function number_validate() {
     if (!found) ok = false;
   }
   if (!ok) {
-    alert("Номер не корректный!");
+    alert('Номер не корректный!');
   } else {
-    alert("Номер корректный!");
+    alert('Номер корректный!');
   }
 }
